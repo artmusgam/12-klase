e29374d55e54a9c6ba5065112c438c09c19d783f
artmusgam <113336755+artmusgam@users.noreply.github.com>
2023-09-13 15:40:48 +0300

    Add files via upload

--- dators.py
+++ dators.py
@@ -1,20 +1,52 @@
-import csv
 
+#minimalas prasibas
 class datora_sastavdala():
+    #konstruktors
     def __init__(self, veids, modelis, cena):
         self.veids = veids
         self.modelis = modelis
         self.cena = cena
-    
+    #informacijas apskate
     def izdrukat(self):
         print("Veids: ", self.veids)
         print("Modelis: ", self.modelis)
         print("Cena: ", self.cena)
+    
+    def labosana(self,veids,modelis,cena):
+        self.veids = veids
+        self.modelis = modelis
+        self.cena = cena
 
     def saglabat(self):
-        with open('sastavdala.csv', 'w',encoding="utf-8",newline='') as fails:
-            csvwrite = csv.writer(fails)
-            csvwrite.writerow(['Veids','Modelis','Cena'])
-            csvwrite.writerow([self.veids, self.modelis, self.cena])
+        with open('sastavdalas.txt', 'w', encoding="utf-8") as fails :
+            fails.write("-datora sastāvdaļas-")
+            fails.write(" \n")
+            fails.write(str(dators.veids))
+            fails.write(" \n")
+            fails.write(str(dators.modelis))
+            fails.write(" \n")
+            fails.write(str(dators.cena))
+
 dators = datora_sastavdala(veids='RAM',modelis= 'Corsair Vengeance LPX 16GB',cena= '99,99 EUR')
-print(dators.veids)
+dators.izdrukat()
+dators.labosana(veids= input("ievadi veidu: "), modelis= input("ievadi modeli: "), cena= input("ievadi cenu: "))
+dators.izdrukat()
+
+#datu saglabašana
+
+dators.saglabat()
+
+import PySimpleGUI as psg
+
+psg.theme('DarkAmber')
+layout = [[psg.Text('Komponents')]]
+
+layout2 = [[psg.Text('Rediģēšana')]]
+
+tabgrp = [
+    psg.TabGroup(
+        [
+            
+        ]
+    )
+]
