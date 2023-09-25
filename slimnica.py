6b869648a930118f712656c0569a5609168bd2b1
artmusgam <113336755+artmusgam@users.noreply.github.com>
2023-09-25 15:23:42 +0300

    Add files via upload

---
+++ slimnica.py
@@ -0,0 +1,12 @@
+class Slimnica():
+    def __init__(self,Pacients_vards,Pacients_uzvards,Pacients_pk,Pacients_tel_numurs,Pacients_nozare,Dakteris_vards,Dakteris_uzvards,Dakteris_tel_numurs,Dakteris_nozare):
+            self.Pacients_vards = Pacients_vards
+            self.Pacients_uzvards = Pacients_uzvards
+            self.Pacients_pk = Pacients_pk
+            self.Pacients_tel_numurs = Pacients_tel_numurs
+            self.Pacients_nozare = Pacients_nozare
+            self.Dakteris_vards = Dakteris_vards
+            self.Dakteris_uzvards = Dakteris_uzvards
+            self.Dakteris_tel_numurs = Dakteris_tel_numurs
+            self.Dakteris_nozare = Dakteris_nozare
+
