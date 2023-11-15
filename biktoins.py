ab56811533135faa4d951d774eb131b7ae9315f0
artmusgam <113336755+artmusgam@users.noreply.github.com>
2023-11-15 15:35:40 +0200

    Add files via upload

---
+++ biktoins.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+URL = "https://api.coindesk.com/v1/bpi/currentprice.json"
+
+atbilde = requests.get(URL)
+
+print(atbilde)
+
+dati = atbilde.json()
+
+print(json.dumps(dati,indent=2))
